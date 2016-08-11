4fa3d1e859ffe20514d6ca366098c9b6b7fb439a
Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>
2016-08-11 17:16:39 -0300

    model.py: use the new 'get_all_model_instances' utils function
    This patch uses the new get_all_model_instances function from
    WoK utils to retrieve all Gingerbase model instances.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>

--- model/model.py
+++ model/model.py
@@ -17,29 +17,15 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-import inspect
-import os
-
 from wok.basemodel import BaseModel
 from wok.objectstore import ObjectStore
 from wok.plugins.gingerbase import config
-from wok.utils import import_module, listPathModules
+from wok.utils import get_all_model_instances, get_model_instances
 from wok.utils import upgrade_objectstore_schema
 
 
 class Model(BaseModel):
     def __init__(self, objstore_loc=None):
-
-        def get_instances(module_name):
-            instances = []
-            module = import_module(module_name)
-            members = inspect.getmembers(module, inspect.isclass)
-            for cls_name, instance in members:
-                if inspect.getmodule(instance) == module and \
-                   cls_name.endswith('Model'):
-                    instances.append(instance)
-
-            return instances
 
         if objstore_loc is None:
             objstore_loc = config.get_object_store()
@@ -51,23 +37,11 @@ class Model(BaseModel):
         self.objstore = ObjectStore(objstore_loc)
         kargs = {'objstore': self.objstore}
 
-        models = []
+        models = get_all_model_instances(__name__, __file__, kargs)
+
         # Import task model from Wok
-        instances = get_instances('wok.model.tasks')
+        instances = get_model_instances('wok.model.tasks')
         for instance in instances:
             models.append(instance(**kargs))
-
-        # Import all Kimchi plugin models
-        this = os.path.basename(__file__)
-        this_mod = os.path.splitext(this)[0]
-
-        for mod_name in listPathModules(os.path.dirname(__file__)):
-            if mod_name.startswith("_") or mod_name == this_mod:
-                continue
-
-            instances = get_instances('wok.plugins.gingerbase.model.' +
-                                      mod_name)
-            for instance in instances:
-                models.append(instance(**kargs))
 
         return super(Model, self).__init__(models)
